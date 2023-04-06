b6cdcb575ce397f50b6f29c9458af0324b328f12
­Amna2017 <aiaashour2017@gmai.com>
2023-04-05 19:50:49 -0700

    my square root commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,6 @@
 /**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to calculate the square root of
- *
  * Return: the resulting square root
  */
 int _sqrt_recursion(int n)
@@ -11,13 +10,11 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
-
 /**
  * actual_sqrt_recursion - recurses to find the natural
  * square root of a number
  * @n: number to calculate the sqaure root of
- * @i: iterator
- *
+ * @g: iterator
  * Return: the resulting square root
  */
 int actual_sqrt_recursion(int n, int g)
