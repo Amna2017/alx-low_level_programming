db65f8d523760a6844edac50f33d4bb2985cefe6
­Amna2017 <aiaashour2017@gmai.com>
2023-04-05 19:45:43 -0700

    my 101 final commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * wildcmp - Compare strings
  * @s1: pointer to string params
  * @s2: pointer to string params
  * Return: 0
  */
-
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
@@ -27,4 +25,4 @@ int wildcmp(char *s1, char *s2)
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
 	return (0);
-}	
+}
