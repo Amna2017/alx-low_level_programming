dea031e796d1b55dab20d2622a487aed15699a69
Amna2017 <asiaashour2017@gmail.com>
2023-04-12 18:49:54 -0700

    mm

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,11 +1,11 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * _strlen - length of a string
  * @s: input char
  * Return: length of a string
  */
-
 int _strlen(char *s)
 {
 	int l = 0;
@@ -17,12 +17,16 @@ int _strlen(char *s)
 	}
 	return (l);
 }
-
 /**
 * str_concat - Concat 2 strings.
 * @s1: string
 * @s2: string
 * Return: char
+* @l1: int
+* @l2: int
+* @malloc: function to save size in memory
+*
+*
 */
 
 char *str_concat(char *s1, char *s2)
@@ -34,22 +38,17 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	else
 		l1 = _strlen(s1);
-
 	if (!s2)
 		s2 = "";
 	else
 		l2 = _strlen(s2);
-
-	conc = malloc(l1 + l2 + 1);
+	conc = (char*)malloc(l1 + l2 + 1);
 	if (!conc)
 		return (0);
-
 	tmp = conc;
 	while (*s1)
 		*tmp++ = *s1++;
-
 	while ((*tmp++ = *s2++))
 		;
-
 	return (conc);
 }
