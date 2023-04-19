07a005702bcd5f5c9f71ebc7b409c9c47591f85c
Amna2017 <asiaashour2017@gmail.com>
2023-04-18 19:30:21 -0700

    m

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,8 +7,16 @@
 * @d: struct dog to print
 */
 
-void print_dog( struct dog *d)
+void print_dog(struct dog *d)
 {
-if(d)
-printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
+
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
 
-/** 
+/**
  * * free_dog - frees memory allocated for a struct dog* @d: struct dog to free
 * @d: structure new name
 */
 
 void free_dog(dog_t *d)
-
 {
 if (d)
 {
