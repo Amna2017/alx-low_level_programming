32b16174730c4a00ef950092ac6c033525abdf67
Amna2017 <asiaashour2017@gmail.com>
2023-05-09 19:47:23 -0700

    mmm

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,13 +9,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
 
 #endif
