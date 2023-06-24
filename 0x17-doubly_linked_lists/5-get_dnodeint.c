6ccdcd8c410f141d86b16e3a415ae33a2e628e93
Amna2017 <asiaashour2017@gmail.com>
2023-06-23 23:00:12 -0700

    0x17-doubly_linked_lists amna

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get number of nodes
+ *
+ *@head : head node
+ *@index : number of nodes
+ * Return: number of nodes
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current = head;
+	unsigned int count = 0;
+
+	while (current != NULL && count < index)
+	{
+		current = current->next;
+		count++;
+	}
+	if (count == index)
+	{
+		return (current);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
