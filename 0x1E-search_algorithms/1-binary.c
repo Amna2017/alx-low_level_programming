bcaed382436547a99dc0c03dd5d956757ea666eb
Amna2011 <amnataha2011@gmail.com>
2023-11-05 18:00:04 -0800

    mmm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,11 +11,50 @@
  */
 int recursive_search(int *array, size_t size, int value)
 {
-	size_t half = size / 2;
-	size_t m;
+        size_t half = size / 2;
+        size_t m;
 
-	if (array == NULL || size == 0)
-		return (-1);
+        if (array == NULL || size == 0)
+                return (-1);
 
-	printf("Searching in array");
+        printf("Searching in array");
 
+        for (m = 0; m < size; m++)
+                printf("%s %d", (m == 0) ? ":" : ",", array[m]);
+
+        printf("\n");
+
+        if (half && size % 2 == 0)
+                half--;
+
+        if (value == array[half])
+                return ((int)half);
+
+        if (value < array[half])
+                return (recursive_search(array, half, value));
+
+        half++;
+
+        return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+ * binary_search - calls to binary_search to return
+ * the index of  number
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int binary_search(int *array, size_t size, int value)
+{
+        int index;
+
+        index = recursive_search(array, size, value);
+
+        if (index >= 0 && array[index] != value)
+                return (-1);
+
+        return (index);
+}
