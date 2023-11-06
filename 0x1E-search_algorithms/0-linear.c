9241236d87feb5a27bf053897939720a05f6255e
Amna2011 <amnataha2011@gmail.com>
2023-11-05 17:57:15 -0800

    mmm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,16 +11,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int m;
+	int n;
 
 	if (array == NULL)
 		return (-1);
 
-	for (m = 0; m < (int)size; m++)
+	for (n = 0; n < (int)size; n++)
 	{
-		printf("Value checked array[%u] = [%d]\n", m, array[m]);
-		if (value == array[m])
-			return (m);
+		printf("Value checked array[%u] = [%d]\n", n, array[n]);
+		if (value == array[n])
+			return (n);
 	}
 	return (-1);
 }
